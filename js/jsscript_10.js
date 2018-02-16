954766c0e119ad8484328d430c01d16bca865974
chamsmarena <chamsmarena@hotmail.com>
2018-02-16 13:03:45 +0000

    Version 2

--- js/jsscript_10.js
+++ js/jsscript_10.js
@@ -1,41 +1,95 @@
 $(document).ready(function(){
+	Winheight = $(window).height();
+	$(".col1").css("height",Winheight);
+	$(".col2").css("height",Winheight);
 	
 	
+	$('.col1').bind('mouseenter', function() {
+		var self = $(this);
+
+		   $('.col1').animate({
+				width:'75%'
+			},150,"swing");
+			$('.col1 .anytime').animate({
+				width:'75%'
+			},150,"swing");
+			$('.col1 .rowTitle2').animate({
+				width:'75%'
+			},150,"swing");
+			$('.col2').animate({
+				width:'25%'
+			},150,"swing");
+			$('.col2 .rowTitle').animate({
+				width:'25%'
+			},150,"swing");
+
+	}).bind('mouseleave', function(){
+		this.iid && clearInterval(this.iid);
+	});
 	
+	$('.col2').bind('mouseenter', function() {
+		var self = $(this);
+	
+		   $('.col2').animate({
+				width:'75%'
+			},150,"swing");
+			$('.col2 .rowTitle').animate({
+				width:'75%'
+			},150,"swing");
+			$('.col1').animate({
+				width:'25%'
+			},150,"swing");
+			$('.col1 .anytime').animate({
+				width:'25%'
+			},150,"swing");
+			$('.col1 .rowTitle2').animate({
+				width:'25%'
+			},150,"swing");
+
+	}).bind('mouseleave', function(){
+		this.iid && clearInterval(this.iid);
+	});
+});
+
+$(window).resize(function() {
+	Winheight = $(window).height();
+	$(".col1").css("height",Winheight);
+	$(".col2").css("height",Winheight);
 });
 
 
-$("#anytimeproduct").hover(function(){
-	$('#anytimeproduct').animate({
+/*
+$(".col1").hover(function(){
+	$('.col1').animate({
 		width:'75%'
 	},150,"swing");
-	$('#anytimeproduct .anytime').animate({
+	$('.col1 .anytime').animate({
 		width:'75%'
 	},150,"swing");
-	$('#anytimeproduct .rowTitle2').animate({
+	$('.col1 .rowTitle2').animate({
 		width:'75%'
 	},150,"swing");
-	$('#scheduledproduct').animate({
+	$('.col2').animate({
 		width:'25%'
 	},150,"swing");
-	$('#scheduledproduct .rowTitle').animate({
-		width:'25%'
-	},150,"swing");
-});
-$("#scheduledproduct").hover(function(){
-	$('#scheduledproduct').animate({
-		width:'75%'
-	},150,"swing");
-	$('#scheduledproduct .rowTitle').animate({
-		width:'75%'
-	},150,"swing");
-	$('#anytimeproduct').animate({
-		width:'25%'
-	},150,"swing");
-	$('#anytimeproduct .anytime').animate({
-		width:'25%'
-	},150,"swing");
-	$('#anytimeproduct .rowTitle2').animate({
+	$('.col2 .rowTitle').animate({
 		width:'25%'
 	},150,"swing");
 });
+$(".col2").hover(function(){
+	$('.col2').animate({
+		width:'75%'
+	},150,"swing");
+	$('.col2 .rowTitle').animate({
+		width:'75%'
+	},150,"swing");
+	$('.col1').animate({
+		width:'25%'
+	},150,"swing");
+	$('.col1 .anytime').animate({
+		width:'25%'
+	},150,"swing");
+	$('.col1 .rowTitle2').animate({
+		width:'25%'
+	},150,"swing");
+});*/
